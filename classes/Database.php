100f54d2f40af7b25c7eeaa1c4956109676ddd91
6784 <6784@ait-sydney.net.au>
2019-07-24 16:47:27 +1000

    first commit

---
+++ classes/Database.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace aitsydney;
+
+class Database {
+
+    protected $connection;
+
+    public function __construct(){
+        $this -> connection = mysqli_connect('localhost', 'website', 'password', 'data');
+    }
+        
+}
+?>
